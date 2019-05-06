79f2bea3a83ecbdc2746621df59a798c1b00c205
bagaking <zfaustk@qq.com>
2019-05-06 19:48:15 +0800

    feat: create turtle

---
+++ src/conf/interfeces.ts
@@ -0,0 +1,7 @@
+export interface IConf {
+    name: string;
+    id: string|number;
+    port?: string|number;
+    drivers?: any;
+    rules?: any;
+}
---
+++ src/turtle/index.ts
@@ -0,0 +1,43 @@
+import * as fs from "fs-extra";
+import * as Path from "path";
+import {InitDrivers} from "../core";
+import {IConf} from "../conf/interfeces";
+import {EventEmitter} from "events";
+
+export class Turtle<IDrivers> {
+
+    public conf: IConf;
+    public confPath: any;
+
+    public get drivers(): IDrivers {
+        return this._drivers as IDrivers;
+    }
+    protected _drivers: any;
+
+    public setConf(path: string, force: boolean) {
+        if (!force && this.conf) {
+            return;
+        }
+
+        path = Path.isAbsolute(path) ? path : Path.resolve(process.cwd(), path);
+        if (!fs.existsSync(path)) {
+            throw new Error(`conf file at path(${path}) cannot be found`);
+        }
+
+        let content = fs.readFileSync(path);
+        try {
+            this.conf = JSON.parse(content.toString());
+        } catch (e) {
+            throw new Error(`parse conf file at path(${path}) failed, content: ${content.toString()}`);
+        }
+        this.confPath = path;
+    }
+
+    public async initialDrivers(drivers: Array<string | Function>, cb?: (e: EventEmitter) => void) {
+        const results = await InitDrivers(this.conf.drivers, _drivers, cb);
+        this._drivers = this._drivers ? {...this._drivers, ...results} : results; // override
+        console.log("\nDRIVERS INITIALED\n");
+    }
+}
+
+export const turtle = new Turtle<any>();
