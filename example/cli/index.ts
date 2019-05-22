96c96c3a5ad3cf385fee53e76b2a3ac495a869d1
bagaking <zfaustk@qq.com>
2019-05-22 19:04:08 +0800

    example: start with cli mode

---
+++ example/cli/index.ts
@@ -0,0 +1,40 @@
+import {CommondLineApp} from "../../src/cli/app";
+import {APIRunningState, IApi} from "../../src";
+
+class ApiClass implements IApi {
+
+    enabled: boolean;
+    runningRequest: number;
+    runningState: APIRunningState;
+
+    constructor() {
+        this.runningState = APIRunningState.PREPARED;
+    }
+
+    async close(): Promise<boolean> {
+        this.runningState = APIRunningState.CLOSING;
+        console.log(`closed`);
+        this.runningState = APIRunningState.CLOSED;
+        return true;
+    }
+
+    async start(port: number): Promise<boolean> {
+        this.runningState = APIRunningState.STARTING;
+        console.log(`listen to ${port}`);
+        this.runningState = APIRunningState.RUNNING;
+        return true;
+    }
+}
+
+const cli = new CommondLineApp("example", "0.0.1", [], [new ApiClass()], {
+        "name": "example",
+        "id": 0,
+        "port": 8080,
+        "drivers": {
+        },
+        "rules": {
+        }
+    }
+);
+
+cli.run();
