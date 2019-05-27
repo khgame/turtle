c10f136dee2ec6d8644d9b709f9571c277e16667
bagaking <zfaustk@qq.com>
2019-05-27 19:45:46 +0800

    feat (driver): add load consul method

--- src/driver/discover/consul.ts
+++ src/driver/discover/consul.ts
@@ -1,6 +1,6 @@
-import * as Consul from "consul";
 import {Driver, IDriver} from "../../core";
 import {turtle} from "../../turtle";
+import * as Consul from "consul";
 
 export interface IConsulConf {
     tags?: string[];
@@ -15,9 +15,25 @@ export interface IConsulConf {
 @Driver("discover/consul")
 export class DiscoverConsulDriver implements IDriver<IConsulConf, any> {
 
+    protected consul: Consul.Consul;
+
+    protected loadConsul() {
+        if (!require) {
+            throw new Error("Cannot load consul. Try to install all required dependencies.");
+        }
+        if (!this.consul) {
+            try {
+                this.consul = require("consul")();
+            } catch (e) {
+                throw new Error("consul package was not found installed. Try to install it: npm install consul --save");
+            }
+        }
+        return this.consul;
+    }
+
     async init(conf: IConsulConf): Promise<any> {
 
-        const consul = Consul();
+        const consul = this.loadConsul();
 
         const check = {
             http: `http://localhost:${turtle.conf.port}/${conf.health.api}`,
