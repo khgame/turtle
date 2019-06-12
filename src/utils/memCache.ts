1041ce06567432582cba52ff2b4415f89f752637
bagaking <zfaustk@qq.com>
2019-06-13 04:01:11 +0800

    feat (utils): memCache - getLoosingCache

--- src/utils/memCache.ts
+++ src/utils/memCache.ts
@@ -1,7 +1,9 @@
 import * as NodeCache from "node-cache";
+import {forMs} from "kht/lib";
 
 export interface IMemCache extends NodeCache {
     lock(key: string, ttl?: number): boolean;
+
     unlock(key: string): boolean;
 
     lockMany(keys: string[], ttl: number): boolean; // must ttl
@@ -10,6 +12,7 @@ export interface IMemCache extends NodeCache {
 
 export function genMemCache(options?: NodeCache.Options): IMemCache {
     const cache = new NodeCache(options);
+
     const ret: any = cache;
 
     ret.lock = (key: string, ttl: number = 0) => {
@@ -23,8 +26,36 @@ export function genMemCache(options?: NodeCache.Options): IMemCache {
         return cache.del(key) === 1;
     };
 
+    ret.getLoosingCache = async (key: string, fnGetVal: (key: string) => Promise<any>, ttl: number = 0) => { // todo: test this
+        const enableKey = "__enable__" + key;
+        const acquireKey = "__acquire__" + key;
+        if (cache.get(enableKey) !== undefined || !ret.lock(acquireKey, 1)) { // 1 query per second
+            let value = cache.get(key);
+            let t = 50;
+            while (value === undefined && t <= 250) { // up to 750 ms
+                await forMs(t);
+                value = cache.get(key);
+                t += 50;
+            }
+
+            if (value) {
+                return value;
+            }
+        }
+        // locked by me or cache failed. when it triggered by cache-failed, racing may happen.
+        try {
+            // update loosing cache
+            let value = await fnGetVal(key);
+            ret.lock(enableKey, ttl);
+            cache.set(key, value, ttl * 2);
+        } catch {
+        }
+        ret.unlock(acquireKey);
+        return;
+    };
+
     ret.lockMany = (keys: string[], ttl: number) => {
-        if (ttl <= 0){
+        if (ttl <= 0) {
             throw new Error("ttl must be set when lock many");
         }
         if (Object.keys(cache.mget(keys)).length > 0) {
