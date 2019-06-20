1be6bd319938c187ee4ca2412853e339d9e45e69
bagaking <zfaustk@qq.com>
2019-06-20 13:31:21 +0800

    feat: create QueryRecorder

---
+++ src/core/utils/queryRecorder.ts
@@ -0,0 +1,21 @@
+export class QueryRecorder {
+
+    records: any = {};
+    statistic: any = {};
+
+    setSlot(queryName: string, timeCostMs: number) {
+        const ms = Date.now();
+        const s = Math.floor(ms / 1000);
+        const m = Math.floor(s / 60);
+        const h = Math.floor(s / 60);
+        (new Date()).getDay()
+        this.records[m] = this.records[m] || {};
+        this.records[m][queryName] = (this.records[m][queryName] || {});
+        this.records[m][queryName]["timeCostMs"] = timeCostMs + (this.records[m][queryName]["timeCostMs"] || 0);
+        this.records[m][queryName]["count"] = 1 + (this.records[s][queryName]["count"] || 0);
+    }
+
+    pulse(queryName: string, timeCostMs: number) {
+        this.setSlot(queryName, timeCostMs);
+    }
+}
