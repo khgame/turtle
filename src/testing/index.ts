9533548b6a94e523add4537d3a3af420d209c067
bagaking <zfaustk@qq.com>
2019-07-12 14:42:59 +0800

    fix (testing): StartUpTurtleOnlyDriver

--- src/testing/index.ts
+++ src/testing/index.ts
@@ -1,7 +1,7 @@
 import {turtle} from "../turtle";
 import {IConf} from "..";
 
-export async function InitTutleLocally(drivers: Array<string | Function>, conf: IConf){
+export async function StartUpTurtleOnlyDriver(drivers: Array<string | Function>, conf: IConf) {
     turtle.conf = conf;
-    await turtle.initialDrivers(this.drivers);
+    await turtle.initialDrivers(drivers);
 }
