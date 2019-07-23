9a932cda7f073284a215d6ac5df93219607ee02c
bagaking <zfaustk@qq.com>
2019-07-23 20:31:54 +0800

    feat (tools): cli - supports mysql driver

--- tools/init.ts
+++ tools/init.ts
@@ -81,6 +81,22 @@ function defaultConf(name: string, port: string, drivers: string[]) {
         rules: {}
     };
 
+    if (drivers.indexOf("mysql") >= 0) {
+        conf.drivers["mysql"] = {
+            type: "mysql",
+            host: "127.0.0.1",
+            port: 3306,
+            username: "root",
+            password: "",
+            database: name,
+            entities: [
+                "src/entity/*"
+            ],
+            synchronize: true,
+            logging: false
+        };
+    }
+
     if (drivers.indexOf("mongo") >= 0) {
         conf.drivers["mongo"] = {
             host: "127.0.0.1",
