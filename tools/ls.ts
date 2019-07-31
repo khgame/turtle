d686e7125ab5c253e13e6f392fc1e47e0bc7607e
bagaking <zfaustk@qq.com>
2019-07-31 22:51:10 +0800

    feat (tools): ls add option -process to show process status

--- tools/ls.ts
+++ tools/ls.ts
@@ -1,16 +1,22 @@
 import {ConsoleHelper} from "kht";
 import * as fs from "fs";
-import {ICmd} from "./_base";
+import {alive, ICmd} from "./_base";
 
-export const ls : ICmd = {
+export const ls: ICmd = {
     desc: "ls <path>",
     args: {
         info: {
             alias: "i",
+            desc: "print runtime info of the turtle process",
+            input: false
+        },
+        process: {
+            alias: "p",
+            desc: "print if the turtle process are active",
             input: false
         }
     },
-    exec: async (path: string, cmd: { info: string }) => {
+    exec: async (path: string, cmd: { info: string, process: string }) => {
         if (!cmd) {
             cmd = path as any;
             path = undefined;
@@ -18,8 +24,25 @@ export const ls : ICmd = {
         const paths = fs.readdirSync(path || ".");
         const turtles = paths
             .filter(p => p.startsWith(".") && p.endsWith(".turtle"))
-            .map(p => p.substr(1, p.length - 8) + (cmd.info ? " " + fs.readFileSync(p, { encoding: "UTF-8"}) : ""));
+            .map(fName => ({
+                name: fName,
+                runtime: JSON.parse(fs.readFileSync(fName, {encoding: "UTF-8"}))
+            })).map(fstat => ({
+                ...fstat,
+                active: alive(fstat.runtime.pid)
+            }));
+        turtles.forEach(t => {
+            const {name, runtime, active} = t;
+            const print: any[] = [name];
+            if (cmd.process) {
+                print.push(active ? `◆ON:${runtime.pid}◆` : `◇OFF:${runtime.pid}◇` );
+            }
+            if (cmd.info) {
+                print.push(runtime);
+            }
 
-        turtles.forEach(t => console.log(t));
+            console.log(...print);
+        });
     }
 };
+
